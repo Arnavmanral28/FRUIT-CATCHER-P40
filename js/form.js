0daaa1a08930633de3f2fab2903f793b21e40b5a
Arnav MANRAL <MAN0065@curric.bayside-sc.wan>
2021-06-28 18:47:58 +1000

    fruit catch

--- js/form.js
+++ js/form.js
@@ -14,18 +14,18 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(350, 50);
+        this.title.position(750, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(550,400);
+        this.input.position(900,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(560,500);
-        this.button.style('width', '200px');
+        this.button.position(900,500);
+        this.button.style('width', '205px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
-        this.reset.position(900, 660);
+        this.reset.position(530, 660);
         this.reset.style('width', '100px');
         this.reset.style('height', '30px');
         this.reset.style('background', 'lightpink');
